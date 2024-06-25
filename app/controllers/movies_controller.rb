265fd36827bb120572440e0cff58e93243b5ebfd
borvux <bennyjoram@email.com>
2024-06-25 10:56:30 -0500

    finished following the video

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,19 @@
 class MoviesController < ApplicationController
+  def new
+    @the_movie = Movie.new
+    render template: "movies/new"
+  end
+
+  def edit
+    the_id = params.fetch("id")
+
+    matching_movies = Movie.where({ :id => the_id })
+
+    @the_movie = matching_movies[0]
+
+    render template: "movies/edit"
+  end
+  
   def index
     matching_movies = Movie.all
 
@@ -18,16 +33,18 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      # redirect_to("/movies/new", { :alert => the_movie.errors.full_messages.to_sentence })
+      # render template: "movies/with_errors"
+      render template: "movies/new"
     end
   end
 
